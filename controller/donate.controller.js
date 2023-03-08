b061301f190c713955d910864e3d043d863db521
Heshperera <it20096434@my.sliit.lk>
2023-03-08 17:27:14 +0530

    updates

--- controller/donate.controller.js
+++ controller/donate.controller.js
@@ -4,8 +4,23 @@ const { StatusCodes } = require("http-status-codes");
 const addDonate = async (req, res) => {
     const donate = await Donate.create(req.body );
     res.status(StatusCodes.CREATED).json({
+        area:donate.area,
         projectNumber:donate.projectNumber,
+        nearestPoint:donate.nearestPoint,
+        
     });
+
+    //get all donations
+const getAllDonates = async (req, res) => {
+  const donates = await Donate.find({ projectNumber: req.params.id });
+
+  if (!donates) {
+    res.status(StatusCodes.OK).json({ msg: "No donations available!" });
+  }
+  res.status(StatusCodes.OK).json(donates);
+};
+
+  // 
 }
 
 module.exports = { addDonate };
